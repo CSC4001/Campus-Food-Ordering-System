5d63329ded8307427dad42dcae1256ee35b488b8
cbdrrtt <117010383@link.cuhk.edu.cn>
2020-04-09 11:40:52 +0800

    upload shop information

--- myproject/src/components/Information.vue
+++ myproject/src/components/Information.vue
@@ -50,27 +50,80 @@
       </el-col>
       <!-- content -->
       <el-col :span="18">
-        <!-- name  -->
+        <!-- information -->
         <el-row>
+          <el-col :span="8" :offset="5">
+            <a-list itemLayout="horizontal" :dataSource="infoData">
+              <a-list-item slot="renderItem" slot-scope="item">
+                <a slot="actions"><el-button type="text" @click="infoFormVisible = true">edit</el-button></a>  
+                <el-dialog title="information" :visible.sync="infoFormVisible">
+                  <el-form :model="infoForm">
+                    <el-form-item label="name">
+                      <el-input v-model="infoForm.name" autocomplete="off"></el-input>
+                    </el-form-item>
+                    <el-form-item label="location">
+                      <el-input v-model="infoForm.location" autocomplete="off"></el-input>
+                    </el-form-item>
+                    <el-form-item label="opening time">
+                      <el-time-picker
+                        is-range
+                        v-model="infoForm.date"
+                        range-separator="to"
+                        start-placeholder="start"
+                        end-placeholder="end"
+                        >
+                      </el-time-picker>
+                      <!-- <el-col :span="7">
+                        <el-time-picker placeholder="choose time" v-model="infoForm.date1" style="width: 100%;"></el-time-picker>
+                      </el-col>
+                      <el-col class="line" :span="2">-</el-col>
+                      <el-col :span="7">
+                        <el-time-picker placeholder="choose time" v-model="infoForm.date2" style="width: 100%;"></el-time-picker>
+                      </el-col> -->
+                    </el-form-item>
+                    <el-form-item label="contact info">
+                      <el-input v-model="infoForm.contact" autocomplete="off"></el-input>
+                    </el-form-item>
+                    <el-form-item label="relative info">
+                      <el-input v-model="infoForm.relative" autocomplete="off"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                      <div class="block">
+                        <span class="demonstration">delivery fee</span>
+                        <el-slider
+                          v-model="infoForm.fee"
+                          :step="1"
+                          :min="0"
+                          :max="5"
+                          show-stops>
+                        </el-slider>
+                      </div>
+                    </el-form-item>
+
+                    <el-form-item>
+                    <el-button @click="infoFormVisible = false">cancel</el-button>
+                    <el-button type="primary" @click="submitForm('form')">confirm</el-button>
+                    </el-form-item>
+                  </el-form>
+                </el-dialog>
+                <a-list-item-meta>
+                  <span slot="title">{{item.title}} : {{item.content}}</span>
+                
+                </a-list-item-meta>
+              </a-list-item>
+            </a-list>
+            <el-button>close shop</el-button>
+            <el-button>open shop</el-button>
+          </el-col>
         </el-row>
-        <!-- location -->
-        <el-row>
-        </el-row>
-        <!-- opening hour -->
-        <el-row>
-        </el-row>
-        <!-- contact -->
-        <el-row>
-        </el-row>
-        <!-- relative information -->
-        <el-row>
-        </el-row>
-        <!-- delivery fee -->
-        <el-row>
-        </el-row>
+
         <!-- apply -->
-        <el-row>
+        <!-- <el-row type="flex" justify="center">
+          <el-button>close shop</el-button>
         </el-row>
+        <el-row type="flex" justify="center">
+          <el-button>open shop</el-button>
+        </el-row> -->
       </el-col>
       <!-- content end -->
     </el-row>
@@ -92,23 +145,57 @@
 
   ];
 
+    const infoData = [
+    {
+      title: 'name',
+      content: 'content',
+    },
+    {
+      title: 'location',
+      content: '',
+    },
+    {
+      title: 'opening time',
+      content: '',
+    },
+    {
+      title: 'contact info',
+      content: '',
+    },
+    {
+      title: 'relative info',
+      content: '',
+    },
+    {
+      title: 'delivery fee',
+      content: '',
+    },
+  ];
+
   export default {
     name: 'Information',
-
     data() {
       return {
         loginFormVisible: false,
-
+        infoFormVisible: false,
         loginForm: {
             email: '',
             password: ''
         },
-        formSearch: {
-          searchtype: '',
-          searchkey: '',
+        infoForm: {
+          name: '',
+          location: '',
+          date: '',
+          // date1: '',
+          // date2: '',
+          contact: '',
+          relative: '',
+          fee: '',
+
 
         },
         siderData,
+        infoData,
 
       };
     },
