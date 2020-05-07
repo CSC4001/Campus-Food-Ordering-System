7df3cc8fe4353fe63066b136db795408b0fc832f
hrbattery <hrbattery19990526@gmail.com>
2020-05-08 00:48:04 +0800

    api: dishes management

--- myproject/src/components/Shop/DishesManagementMain.vue
+++ myproject/src/components/Shop/DishesManagementMain.vue
@@ -9,70 +9,93 @@
           <el-input v-model="formSearch.searchkey"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button @click="onSubmit">Search</el-button>
+          <el-button>Search</el-button>
         </el-form-item>
       </el-form>
     </el-row>
     <!-- add dishes -->
     <el-row type="flex" justify="center">
-      <el-button @click="dishesFormVisible = true">add dishes</el-button>
+      <el-button @click="dishesFormVisible = true" style='margin-bottom:20px;'>add dishes</el-button>
       <el-dialog title="dishes information" :visible.sync="dishesFormVisible">
         <el-form ref="form" :model="dishesForm" label-width="80px">
-          <el-form-item label="name">
+          <el-form-item label="Name">
             <el-input v-model="dishesForm.name"></el-input>
           </el-form-item>
-          <el-form-item label="price">
+          <el-form-item label="Price">
             <el-input v-model="dishesForm.price"></el-input>
           </el-form-item>
-          <el-form-item label="discription">
-            <el-input v-model="dishesForm.discription"></el-input>
+          <el-form-item label="Discription">
+            <el-input v-model="dishesForm.info"></el-input>
           </el-form-item>
           <el-form-item>
-            <el-button @click="loginFormVisible = false">cancel</el-button>
-            <el-button type="primary" @click="submitForm('form')">create</el-button>
+            <el-button @click="dishesFormVisible = false">Cancel</el-button>
+            <el-button type="primary" @click="submitDishForm('form')">Submit</el-button>
           </el-form-item>
         </el-form>
       </el-dialog>
     </el-row>
     <!-- dishes  -->
     <el-row>
-      <div
-      class="demo-infinite-container"
-      v-infinite-scroll="handleInfiniteOnLoad"
-      :infinite-scroll-disabled="busy"
-      :infinite-scroll-distance="10"
-      >
-        <a-list :dataSource="data" :grid="{ column: 4 }">
+      <div class="demo-infinite-container">
+        <a-list :dataSource="dishesData" :grid="{ column: 2 }">
           <a-list-item slot="renderItem" slot-scope="item">
-            <el-card class="box-card" >
+            <el-card class="box-card">
               <div slot="header" class="clearfix">
-                <span>{{item.name.last}}</span>
-                <el-button style="float: right; padding: 3px 0" type="text">delete</el-button>
+                <span>{{item.name}}</span>
+                <el-button style="float: right; padding: 3px 0" type="text"  @click='dishDetailHandle(item)'>Edit</el-button>
+                <el-popconfirm
+                  confirmButtonText='Yes'
+                  cancelButtonText='Cancel'
+                  icon="el-icon-info"
+                  iconColor="red"
+                  title="Are you sure to delete the dish?"
+                  @onConfirm="dishDelete(item.productid)"
+                >
+                <el-button style="float: right; padding: 3px 5px" type="text" slot="reference">Delete</el-button>
+                </el-popconfirm>
               </div>
-            <!-- <div v-for="o in 4" :key="o" class="text item">
-            {{'列表内容 ' + o }}
-            </div> -->
+              <span>{{item.info}}</span>
               <el-row type="flex" justify="space-between">
-                <span>sales</span>
-                <span>price</span>
+                <span>Sales: {{item.sale}}</span>
+                <span>Price: {{item.price}}</span>
               </el-row>
             </el-card>
             <br/>
           </a-list-item>
-          <div v-if="loading && !busy" class="demo-loading-container">
-            <a-spin/>
-          </div>
         </a-list>
       </div>
     </el-row>
+    <el-dialog
+      title="Edit dishes"
+      :visible.sync="editFormVisible"
+      width="50%"
+      @close="editFormVisible=false">
+      <el-form :model="editForm" ref="editModel" label-width="160px">
+        <el-form-item label="Dish name">
+          <el-input v-model="editForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="Dish price">
+          <el-input v-model.number="editForm.price"></el-input>
+        </el-form-item>
+        <el-form-item label="Dish discription">
+          <el-input v-model="editForm.info"></el-input>
+        </el-form-item>
+      </el-form>
+      
+      <span slot="footer">
+        <el-button @click="editFormVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="submitDishEdit('editModel')">Confirm</el-button>
+      </span>
+    </el-dialog>
+    
   </el-col>
 </el-row>
 </template>
 
 <script>
-  import reqwest from 'reqwest';
+  import Vue from 'vue'
+  // import reqwest from 'reqwest';
   import infiniteScroll from 'vue-infinite-scroll';
-  const fakeDataUrl = 'https://randomuser.me/api/?results=5&inc=name,gender,email,nat&noinfo';
 
   export default {
     name: 'DishesManagementMain',
@@ -80,13 +103,19 @@
     data() {
       return {
         dishesFormVisible: false,
-        data: [],
-        loading: false,
-        busy: false,
-        dishesForm: {
+        editFormVisible: false,
+        dishesData: [],
+        editForm: {
           name: '',
           price: '',
-          discription: '',
+          info: '',
+        },
+        dishesForm: {
+          shopid: '',
+          userid: '',
+          name: '',
+          price: '',
+          info: '',
         },
         formSearch: {
           searchtype: '',
@@ -95,38 +124,121 @@
       };
     },
     beforeMount() {
-      this.fetchData(res => {
-        this.data = res.results;
-      });
+      // this.fetchData(res => {
+      //   this.dishesData = res.results;
+      // });
+      Vue.axios.get('/api/getDishes', {
+        params: {
+          user_id: sessionStorage.getItem('accessToken'),
+          shop_id: sessionStorage.getItem('shop_id')
+        }
+      }).then((response) => {
+        var data = response.data
+        this.dishesData = data
+      })
     },
     methods: {
-      submitForm(formName) {
+      submitDishForm(formName) {
+        this.$data.dishesForm['shopid'] = sessionStorage.getItem('shop_id')
+        this.$data.dishesForm['userid'] = sessionStorage.getItem('accessToken')
         this.$refs[formName].validate((valid) => {
           if (valid) {
-            alert('submit!');
+            fetch("/api/submitDish",{
+              method:"post",
+              headers:{
+                'content-type':'application/json'
+              },
+              body: JSON.stringify(this.dishesForm)
+            }).then((r) => {
+              return r.json()
+            }).then((data) => {
+              console.log(data)
+              if (data.status == 'ok'){
+                this.$message.success('Add dish success!');
+                this.$data.dishesFormVisible = false;
+                setTimeout(() =>{
+                  location.reload();
+                },1000);
+              } else {
+                this.$message.error('Add dish failed.');
+                return false;
+              }
+            })
           } else {
-            console.log('error submit!!');
+            this.$message.error('Error submit!!');
             return false;
           }
         });
       },
-      onSubmit() {
-        console.log('submit!');
-      },
-      
-      fetchData(callback) {
-        reqwest({
-          url: fakeDataUrl,
-          type: 'json',
-          method: 'get',
-          contentType: 'application/json',
-          success: res => {
-            callback(res);
-          },
+      submitDishEdit(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            fetch("/api/editDish",{
+              method:"post",
+              headers:{
+                'content-type':'application/json'
+              },
+              body: JSON.stringify(this.editForm)
+            }).then((r) => {
+              return r.json()
+            }).then((data) => {
+              console.log(data)
+              if (data.status == 'ok'){
+                this.$message.success('Edit dish success!');
+                this.$data.editFormVisible = false;
+                setTimeout(() =>{
+                  location.reload();
+                },1000);
+              } else {
+                this.$message.error('Edit dish failed.');
+                return false;
+              }
+            })
+          } else {
+            this.$message.error('Error submit!!');
+            return false;
+          }
         });
       },
+      dishDelete(id) {
+        let form = {
+          userid: sessionStorage.getItem('accessToken'),
+          shopid: sessionStorage.getItem('shop_id'),
+          productid: id
+        }
+        fetch("/api/deleteDish",{
+          method:"post",
+          headers:{
+            'content-type':'application/json'
+          },
+          body: JSON.stringify(form)
+        }).then((r) => {
+          return r.json()
+        }).then((data) => {
+          console.log(data)
+          if (data.status == 'ok'){
+            this.$message.success('Delete dish success!');
+            this.$data.editFormVisible = false;
+            setTimeout(() =>{
+              location.reload();
+            },1000);
+          } else {
+            this.$message.error('Eelete dish failed.');
+            return false;
+          }
+        })
+      },
+      dishDetailHandle(item) {
+        this.editFormVisible = true
+        for (let key in item) {
+          this.editForm[key] = item[key]
+        }
+        console.log('im working')
+        this.editForm['shopid'] = sessionStorage.getItem('shop_id')
+        this.editForm['userid'] = sessionStorage.getItem('accessToken')
+      },
       handleInfiniteOnLoad() {
-        const data = this.data;
+        const data = this.dishesData;
         this.loading = true;
         if (data.length > 14) {
           this.$message.warning('Infinite List loaded all');
@@ -134,10 +246,6 @@
           this.loading = false;
           return;
         }
-        this.fetchData(res => {
-          this.data = data.concat(res.results);
-          this.loading = false;
-        });
       },
     },
 
@@ -173,10 +281,6 @@
   }
   .clearfix:after {
     clear: both
-  }
-
-  .box-card {
-    width: 180px;
   }
 
 </style>
