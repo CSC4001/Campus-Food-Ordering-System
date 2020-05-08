039789c242a7d43bb20ab3b9385d289e20e5ed4d
hrbattery <hrbattery19990526@gmail.com>
2020-05-09 02:57:19 +0800

    api: constructing page

--- myproject/src/components/Order/Shop.vue
+++ myproject/src/components/Order/Shop.vue
@@ -9,7 +9,21 @@
           <Sidebar/> <!-- Fixed Sidebar-->
         </el-aside>
         <el-main>
+          <!--h3>{{shopData.name}}</h3-->
+          <h1>Name</h1>
+          <el-col :span='2'>
+            <p>ID:</p>
+          </el-col>
+          <el-col :span='2'>
+            <el-button type="text" @click="favourite()">Favoutite</el-button>
+          </el-col>  
+          <el-col>
+            <el-row>
+              <el-col :span="10"></el-col>
 
+              <el-col :span="10"></el-col>
+            </el-row>
+          </el-col>
         </el-main>
       </el-container>
     </el-container>
@@ -17,7 +31,7 @@
 </template>
 
 <script>
-import Vue from 'vue'
+// import Vue from 'vue'
 import Sidebar from '@/components/UserInterface/Sidebar'
 import Header from '@/components/UserInterface/Header'
 
